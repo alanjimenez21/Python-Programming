ad1f64de3f5294f22a8089a1b2f4787617168307
Alan Jimenez Gallardo <58850933+alanjimenez21@users.noreply.github.com>
2020-10-24 16:17:42 -0400

    Create Lab01.py

---
+++ Computer_Fundamentals_I/Lab01.py
@@ -0,0 +1,46 @@
+"""
+*This program will ask the user to input three integers and calculate sum, product,  max, min, average, and more.
+"""
+
+# Prompt User to input three integers
+
+x = int(input("Please enter integer: "))
+y = int(input("Please enter another integer: "))
+z = int(input("Please enter the third integer: "))
+
+# Calculations
+sumOfIntegers = x + y + z
+productResult = x * y * z
+maxValue = max(x, y, z)  # MAX function similar function for min
+minValue = min(x, y, z)
+averageOfIntegers = sumOfIntegers / 3
+sumRaisedToPower = sumOfIntegers ** minValue
+sumDividedByMinimum = sumOfIntegers // minValue
+sumDividedByMaximum = sumOfIntegers // maxValue
+averageOfMaxAndMin = (maxValue + minValue) / 2
+sortedIntegres = minValue, sumOfIntegers - (maxValue + minValue), maxValue
+name = "Alan Ivan Jimenez Gallardo"
+major = "Information Technology: Computer Programming & Development."
+reason = "I am taking CSC-120 because I want to have a strong foundation on computer science."
+plan = "I want to be a Software Engineer with my M.S. in Computer Science in a company that innovates, pursues high quality products, and creates an impact in society."
+
+# Display all calculations
+print()
+print("1-The numbers entered are ", x, " and", y, " and ", z)
+print("2-Sum = ", sumOfIntegers)
+print("3-Product = ", productResult)
+print("4-Maximum = ", maxValue)
+print("5-Minimum = ", minValue)
+print("6-Average = ", averageOfIntegers)
+print("7-Sum to the power of Min Value = ", sumRaisedToPower)
+print("8-Sum divided by minimum = ", sumDividedByMinimum)
+print("9-Sum divided by maximum = ", sumDividedByMaximum)
+print("10-Average of largest and smallest integers = ", averageOfMaxAndMin)
+print("11-Sorted numbers = ", sortedIntegres)
+
+# Display My Information
+print()
+print("Name: ", name)
+print("Major: ", major)
+print("Reasons on taking CSC-120: ", reason)
+print("Plan for 5 years from now: ", plan)
